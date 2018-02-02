9a76bf529b312bccba0d9e797b007476bced91f7
jdhe1120 <jhe1120@gmail.com>
2018-02-02 11:25:43 -0500

    recreate errors

--- week1/debugging/errors.ml
+++ week1/debugging/errors.ml
@@ -6,17 +6,16 @@
 (* Function 1: increments all integers in a list *)
 let rec increment_lst (lst : int list) : int list =
   match lst with
-  | [] -> []
   | head :: tail -> (head + 1) :: (increment_lst tail);;
 
 (* Function 2: finds length of a given list *)  
-let rec length (lst : 'a list) : int =
+let length (lst : 'a list) : int =
     match lst with
     | [] -> 0
-    | _ :: tail -> 1 + length tail;;
+    | head :: tail -> 1 + length tail;;
 
 (* Function 3: finds the sum of a list of floats *)
-let rec float_sum (lst : float list) : float =
+let float_sum (lst : float list) : float =
     match lst with
     | [] -> 0.0
-    | head :: tail -> head +. (float_sum tail);; 
+    | head :: tail -> h + (sum tail);; 
